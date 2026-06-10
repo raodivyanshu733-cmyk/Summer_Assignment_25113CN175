638d98bac425438782e5577be48c0f21cd56ab24
Divyanshu Rao <raodivyanshu733@gmail.com>
2026-06-10 13:38:35 +0530

    Add files via upload

---
+++ Day-10/Programm-2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int i,j,rows;
+
+    printf("Enter number of rows:");
+    scanf("%d",&rows);
+
+    for(i=0;i<rows;i++){
+        for(j=0;j<i;j++){
+            printf(" ");
+        }
+        for(j=0;j<2*(rows-i)-1;j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
